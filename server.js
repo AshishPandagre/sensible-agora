6bc9615ad398c514e7eeed84ce54dd40168b016b
AshishPandagre <ashishpandagre9@gmail.com>
2022-08-28 10:17:06 +0530

    socket io setup

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 
 app.set('views', path.join(__dirname, 'views'))
 app.use('public/', express.static(path.join(__dirname, 'public')))
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 
 
 app.get('/:room', (req, res) => {
@@ -14,7 +26,7 @@ app.get('/:room', (req, res) => {
 
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
-	console.log(`SERVING ON PORT ${PORT}`)
+server.listen(PORT, () => {
+    console.log(`SERVING ON PORT ${PORT}`)
 })
 
